f59fca31d706cfec1d2a09ade459706af2931823
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-13 18:51:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,9 +58,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    for (var i = cart.length; i>= 0; i--) {
-        cart.pop();
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.length = 0;
   }
   else {
     console.log("Sorry, we dont have a credit card on file for you.");

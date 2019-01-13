9b918d5ce1919344ab06d32b9302a441c91d2cb4
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-13 18:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,14 +57,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  if(cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     for (var i = cart.length; i>= 0; i--) {
       cart.pop();
-    }
-  }
-  else {
-    console.log("Sorry, we don\'t have a credit card on file for you.");
-  }
+    } 
+    } else 
+      console.log('Sorry, we don\'t have a credit card on file for you.')
 return cart;
-}
+  }

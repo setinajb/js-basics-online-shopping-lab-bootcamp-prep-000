bffdd2e0ede435427aba1c0526a685666a3af230
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-13 18:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
     cart.length = 0;
-    return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`);
+    return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     
   }
   else {

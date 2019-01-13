bd9896638e846cb25e0034cb7e07e9034f8c4828
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-13 18:35:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,5 +57,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+  
+    
+  }
+  else {
+  console.log("Sorry, we don't have a credit card on file for you.") 
+  }
 }
